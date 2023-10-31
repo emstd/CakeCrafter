5dac11dc2a300e8474d4913551cb4cdbd741eb58
emstd <49056473+emstd@users.noreply.github.com>
2023-10-31 23:36:17 +0700

    Testing model validate

--- CakeCrafter.API/Models/CakesIngredients.cs
+++ CakeCrafter.API/Models/CakesIngredients.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.Identity.Client;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace CakeCrafter.API.Models
 {
     public class CakesIngredients
     {
-        public int DishId { get; set; }
-        public Cake Dish { get; set; }
+        [Key, Column(Order = 1)]
+        public int CakeId { get; set; }
+        public Cake Cake { get; set; }
+
+        [Key, Column(Order = 2)]
         public int IngredientId { get; set; }
         public Ingredient Ingredient { get; set; }
         public int IngredientQuantity { get; set; }
