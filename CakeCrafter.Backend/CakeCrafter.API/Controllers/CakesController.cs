57335d5736c428d470946953fa879ee77a5b6fb6
emstd <evg.merinov@mail.ru>
2024-03-17 13:37:40 +0700

    Added few tests for bl and api cake

--- CakeCrafter.Backend/CakeCrafter.API/Controllers/CakesController.cs
+++ CakeCrafter.Backend/CakeCrafter.API/Controllers/CakesController.cs
@@ -46,8 +46,12 @@ namespace CakeCrafter.API.Controllers
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<CakeGetResponse>> GetCakeById(int id)
+        public async Task<ActionResult> GetCakeById(int id)
         {
+            if (id == 0 || id < 0)
+            {
+                return BadRequest();
+            }
             var imageSettings = _imageHostSettings.Value;
             var cake = await _service.GetById(id);
             if (cake == null)
---
+++ CakeCrafter.Backend/CakeCrafter.Tests/API/CakesControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using CakeCrafter.API.Controllers;
+using CakeCrafter.API.Options;
+using CakeCrafter.Core.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Moq;
+
+namespace CakeCrafter.Tests.API
+{
+    public class CakesControllerTests
+    {
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        public async Task GetById_ShouldReturnNotFoundAsync(int id)
+        {
+            //Arrange
+            var service = new Mock<ICakeService>();
+            var mapper = new Mock<IMapper>();
+            var settings = new Mock<IOptions<ImageHostSettings>>();
+
+            CakesController cakesController = new CakesController(service.Object, mapper.Object, settings.Object);
+
+            //Act
+            var result = await cakesController.GetCakeById(id);
+
+            //Assert
+            Assert.IsType<BadRequestResult>(result);
+        }
+    }
+}
